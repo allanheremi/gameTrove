51b19a7e4ac3634d2144404726535cad2cfe8b64
Allan Heremi <allanheremidev@gmail.com>
2023-10-21 12:34:31 +0200

    Fix: Imports

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import Aside from './components/body/Aside';
+import Body from './components/body/Body';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Body/>
     </>
   );
 }
